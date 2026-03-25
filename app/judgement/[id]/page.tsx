0297d1ec814fbfc77f4d4807296118c1dc3379f0
princethila <33749524+princethila@users.noreply.github.com>
2026-03-25 19:31:24 +0200

    fixed judgement/[id]/page.tsx is not a module

--- app/judgement/[id]/page.tsx
+++ app/judgement/[id]/page.tsx
@@ -2,12 +2,14 @@
 
 import { useRouter, useParams } from "next/navigation";
 
-export default function AnalysisPage({ params }: { params: Promise<{ id: string }> }){
+export default function AnalysisPage(){
     const router = useRouter();
+    const params = useParams();
+    const id = params.id;
 
     return (
         <div>
-            <h1>Analysis Page</h1>
+            <h1>Analysis Page for {id}</h1>
             <button onClick={() => router.push("/")}>Go Back</button>
         </div>
     );
